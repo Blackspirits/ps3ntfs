7540ed56cea2771b6f5c8b1c4de2e4c94ec68e66
jjolano <jjolano@me.com>
2017-03-30 16:37:47 -0400

    adjust header

--- ps3ntfs.h
+++ ps3ntfs.h
@@ -1,16 +1,16 @@
 #ifndef __PS3NTFS_INC__
 #define __PS3NTFS_INC__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stddef.h>
-#include <sys/synchronization.h>
+#include <sys/prx.h>
 
 #include "ntfs.h"
 
 #define ps3ntfs_running() (sys_prx_get_module_id_by_name("NTFSD", 0, NULL) > 0)
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 ntfs_md* ps3ntfs_prx_mounts(void);
 int ps3ntfs_prx_num_mounts(void);
